6465be5490b4f10027ff74f59303ee8b2592cea6
Diana Mora <morra.dianna@gmail.com>
2021-05-13 16:58:21 -0400

    test commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# Brewery.create(name: "Harlem Blue", 
-# location: "New York, NY" , 
-# hours: "n/a", 
-# phone_number: "844-LUV-BEER", 
-# img_url: "")
+Brewery.create(name: "Sankofa Beer Company", 
+location: "Washington, D.C." , 
+hours: "n/a", 
+contact: "info@sankofabeer.com", 
+img_url: "https://lh3.googleusercontent.com/wWi7i6SQq7Vi5C2tIA97iw23NZuqECUT4eUqCLZ12umvekEM_yqPnrl5aKgfWDOG6rOVv1Jgmx83lzMtVyCxQrg_Y9F38w=s750",
+website: "https://www.sankofabeer.com/")
+
